6314075477ad9c46b5fe58046f52bf13c5093cc6
Avi Parshan <avipars@users.noreply.github.com>
2024-08-13 21:08:32 +0300

    Update __init__.py

--- ctf/__init__.py
+++ ctf/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
-
-from ctf import routes
-
-# from ctf.error_handlers import register_error_handlers  # Adjust the
-# import based on your project structure
+# from ctf.error_handlers import register_error_handlers  # Adjust the import based on your project structure
 
 app = Flask(
     __name__,
@@ -14,3 +10,5 @@ app = Flask(
 
 # register_error_handlers(app)
 # app.config['referrer_policy'] = 'strict-origin-when-cross-origin'
+
+from ctf import routes
